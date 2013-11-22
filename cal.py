a19024e84d38e5bca77d8f28c03bd8dab3e93644
Aaron Oppenheimer <aoppenheimer@daktaridx.com>
2013-11-21 22:50:46 -0500

    added date display to events page, just to see how that works

--- cal.py
+++ cal.py
@@ -1,4 +1,5 @@
 from requestmodel import *
+import datetime
 
 def get_calendar_list():
     calendarList = service.calendarList().list().execute(http=decorator.http())
@@ -8,8 +9,8 @@ def get_calendar_from_calendar_id(the_calendar_id):
     calendar = service.calendars().get(calendarId=the_calendar_id).execute(http=decorator.http())
     return calendar
 
-def get_events_from_calendar_id(the_calendar_id):
-    eventList = service.events().list(calendarId=the_calendar_id).execute(http=decorator.http())
+def get_events_from_calendar_id(the_calendar_id, the_start_date=None, the_end_date=None):
+    eventList = service.events().list(calendarId=the_calendar_id, timeMin=the_start_date, timeMax=the_end_date).execute(http=decorator.http())
     return eventList
 
 class CalListHandler(BaseHandler):
@@ -29,13 +30,47 @@ class EventListHandler(BaseHandler):
         the_cal_id = self.request.get("calid",None)
         if the_cal_id is None:
             return # todo figure out what to do
-            
+
+        month_str=self.request.get("m",None)
+        year_str=self.request.get("y",None)
+        if month_str==None or year_str==None:
+            start_date = datetime.datetime.now()
+        else:
+            delta_str=self.request.get("d",0)
+            delta=int(delta_str)
+            year=int(year_str)
+            month=int(month_str)
+            month=month+delta
+            if month>12:
+                month = 1
+                year = year+1
+            if month<1:
+                month=12
+                year = year-1
+            start_date = datetime.datetime(year=year, month=month, day=1)
+            month=month+1
+            if month>12:
+                month = 1
+                year = year+1
+            if month<1:
+                month=12
+                year = year-1
+            end_date = datetime.datetime(year=year, month=month, day=1)
+                        
+        iso_start_date= start_date.strftime("%Y-%m-%dT%H:%M:%S-05:00")
+        iso_end_date= end_date.strftime("%Y-%m-%dT%H:%M:%S-05:00")
+        
         the_calendar = get_calendar_from_calendar_id(the_cal_id)
-        the_event_list = get_events_from_calendar_id(the_cal_id)
+        the_event_list = get_events_from_calendar_id(the_cal_id,
+                                                     the_start_date = iso_start_date, 
+                                                     the_end_date = iso_end_date)
         
         template_args = {
             'title' : "Event List",
             'the_calendar' : the_calendar,
-            'the_event_list' : the_event_list['items']
+            'the_event_list' : the_event_list['items'],
+            'the_month_string' : start_date.strftime("%b, %Y"),
+            'the_month' : start_date.month,
+            'the_year' : start_date.year,
         }
         self.render_template('calendar_events.html', template_args)
